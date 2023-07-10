3f56316d622b190c13c728db798d237f0f03eb35
Omar Ibrahim <51462341+omargfh@users.noreply.github.com>
2023-07-09 22:12:06 -0500

    Create colored_print_messages.py

---
+++ colored_print_messages.py
@@ -0,0 +1,4 @@
+error = lambda msg: print(f"\033[91m Error:\t\t \033[0m{msg}")
+info = lambda msg: print(f"\033[94m INFO:\t\t \033[0m{msg}")
+success = lambda msg: print(f"\033[92m Success:\t\t \033[0m{msg}")
+warning = lambda msg: print(f"\033[93m Warning:\t\t \033[0m{msg}")
