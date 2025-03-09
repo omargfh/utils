40e86f9a6d853ddda2a65f4834e33f3522c94f95
Omar Ibrahim <51462341+omargfh@users.noreply.github.com>
2025-03-09 00:27:39 -0600

    Update print.py

--- print.py
+++ print.py
@@ -1,27 +1,35 @@
+from typing import Optional
+
 class PrintUtils:
     def __init__(self):
         pass
 
     @staticmethod
-    def error(msg: str):
-        print(f"\033[91m Error:\t\t \033[0m{msg}")
+    def error(*args, **kwargs):
+        msg = " ".join(args)
+        print(f"\033[91m Error:\t\t \033[0m{msg}", **kwargs)
 
     @staticmethod
-    def info(msg: str):
-        print(f"\033[94m INFO:\t\t \033[0m{msg}")
+    def info(*args, **kwargs):
+        msg = " ".join(args)
+        print(f"\033[94m INFO:\t\t \033[0m{msg}", **kwargs)
 
     @staticmethod
-    def success(msg: str):
-        print(f"\033[92m Success:\t\t \033[0m{msg}")
+    def success(*args, **kwargs):
+        msg = " ".join(args)
+        print(f"\033[92m Success:\t\t \033[0m{msg}", **kwargs)
 
     @staticmethod
-    def warning(msg: str):
-        print(f"\033[93m Warning:\t\t \033[0m{msg}")
+    def warning(*args, **kwargs):
+        msg = " ".join(args)
+        print(f"\033[93m Warning:\t\t \033[0m{msg}", **kwargs)
 
+
+class FormattedText:
     @staticmethod
     def progress(i: int, total: int):
-        PrintUtils.info(f"[{i}/{total}]")
+        return f"[{i}/{total}]"
 
     @staticmethod
     def percent(i: int, total: int):
-        PrintUtils.info(f"[{(i/total)*100:.2f}%]")
+        return f"[{(i/total)*100:.2f}%]"
